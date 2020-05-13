88e5637a2bd4eb2aeb97ec812c0d559a363b6a17
fumiya-ozaki <fumiya0414@gmail.com>
2020-05-13 18:27:31 +0900

    5月13日修正

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
 var app = new Vue({
   el:'#app',
   data:{
-    message: "Hello"
+    //search関連
+    items:[],
+    keyword:'',
+    //review関連
+    reviews:[],
+    reviewDate:"",
+    comment:"",
+
+    books:[
+      {
+        no:1,
+        title:"これから始めるVue.js入門",
+        author:"山田祥寛",
+        publisher:"SBクリエイティブ",
+        releaseDate:"2019-08-30",
+        path:"./img/cat_3.jpeg"
+      },
+      {
+        no:2,
+        title:"これから始めるWordpress入門",
+        author:"ワード一郎",
+        publisher:"SBクリエイティブ",
+        releaseDate:"2019-01-02",
+        path:"./img/cat_3.jpeg"
+      }
+    ],
+  },
+  methods:{
+    //reviewの登録
+    addReview:function(event){
+      if(this.comment=='' || this.reviewDate=='')return;
+      var newReview={
+        title: this.books[0].title,
+        date: this.reviewDate,
+        comment: this.comment
+      };
+      this.reviews.push(newReview);
+      this.reviewDate = '';
+      this.comment='';
+    },
+    //本の検索
+    getResolt(keyword){
+      axios.get("https://www.googleapis.com/books/v1/volumes?q=search" + query).then(response => {
+        console.log(response.data);
+        this.items = response.data.items;
+      });
+    }
   }
 })
